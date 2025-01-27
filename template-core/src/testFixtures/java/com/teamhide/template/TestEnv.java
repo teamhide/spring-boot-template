1dfd0efbb0f73c610e8145203d1a1b91dde95fed
Hide <padocon@naver.com>
2025-01-27 16:06:41 +0900

    feat: Fix RepositoryTest annotation

---
+++ template-core/src/testFixtures/java/com/teamhide/template/TestEnv.java
@@ -0,0 +1,8 @@
+package com.teamhide.template;
+
+public class TestEnv {
+    public static final String DOMAIN = "domain-test";
+    public static final String APPLICATION = "application-test";
+    public static final String PERSISTENCE = "persistence-test";
+    public static final String TEST = "test";
+}
--- template-infra/persistence/src/test/java/com/teamhide/template/persistence/test/RepositoryTest.java
+++ template-infra/persistence/src/test/java/com/teamhide/template/persistence/test/RepositoryTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @DataJpaTest
-@ActiveProfiles("domain")
+@ActiveProfiles("persistence-test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ImportAutoConfiguration(DataSourceConfig.class)
 @Import(RepositoryTestConfig.class)
